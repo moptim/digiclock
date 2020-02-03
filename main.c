52377055d213c295c22453ee76124cf1681c9cc9
Emmi <emmi@tbd>
2020-02-03 02:06:45 +0200

    continuing

--- main.c
+++ main.c
@@ -8,6 +8,23 @@
 #define ADC_FREQ		100
 #define TMR_RELOAD		(256 - ((TMR_FREQ) / (IRQ_FREQ)))
 
+// 120 for North Americans
+#define ZC_FREQ			100
+
+// For 4 kHz
+#define BTN_PRESCALER_MASK	0xf
+// 32 kHz
+#define ZC_PRESCALER_MASK	0x1
+
+// Note that these use different sampling frequencies
+#define ZC_ALPHA		32
+#define BTN_ALPHA		16
+
+// Thresholds for deciding that a digital IO pin appears to have gone
+// low to high, or high to low
+#define DIGITAL_LO2HI_THRES	200
+#define DIGITAL_HI2LO_THRES	50
+
 #define ADC_INTERVAL		((IRQ_FREQ) / (ADC_FREQ))
 
 // Stolen from the Linux kernel sources. These will probably not work for
@@ -15,7 +32,71 @@
 #define TIME_AFTER(a,b)		(!!(((b) - (a)) & 0x8000))
 #define TIME_BEFORE(a,b)	TIME_AFTER(b, a)
 
-static volatile uint16_t ticks;
+#define PIN_TO_U8(pins,n)	(((pins) & (1 << (n))) ? 0xff : 0)
+
+struct rnd_state {
+	uint8_t a, b, c, x;
+};
+
+static uint8_t  zc_confidence, btn0_confidence, btn1_confidence;
+static uint8_t  zc_state, btn0_state, btn1_state;
+static uint16_t ticks;
+static uint16_t zero_crossings;
+static uint16_t btn0_presses;
+static uint16_t btn1_presses;
+
+// Hours high digit, hours low digit, minutes high digit, minutes low digit,
+// seconds in binary
+static uint8_t  time[5];
+
+static uint8_t *const hrs_hi  = time[0];
+static uint8_t *const hrs_lo  = time[1];
+static uint8_t *const mins_hi = time[2];
+static uint8_t *const mins_lo = time[3];
+static uint8_t *const secs    = time[4];
+
+struct rnd_state rnd;
+
+static void advance_hour(void)
+{
+	if (++(*hrs_lo) >= 10) {
+		*hrs_lo = 0;
+		if (++(*hrs_hi) >= 6) {
+			*hrs_hi = 0;
+		}
+	}
+}
+
+static void advance_minute(void)
+{
+	if (++(*mins_lo) >= 10) {
+		*mins_lo = 0;
+		if (++(*mins_hi) >= 6) {
+			*mins_hi = 0;
+			advance_hour();
+		}
+	}
+}
+
+static void advance_second(void)
+{
+	if (++(*secs) >= 60) {
+		*secs = 0;
+		advance_minute();
+	}
+}
+
+static inline void rerandom(void)
+{
+	rndnum ^= rndnum << 13;
+	rndnum ^= rndnum >> 17;
+	rndnum ^= rndnum << 5;
+
+	rnd.x++;
+	rnd.a = (rnd.a ^ rnd.c ^ rnd.x);
+	rnd.b = (rnd.b + rnd.a);
+	rnd.c = (rnd.c + (rnd.b >> 1) ^ rnd.a);
+}
 
 static uint8_t exponential_filter(uint8_t old, uint8_t new, uint8_t alpha)
 {
@@ -36,16 +117,83 @@ static uint8_t exponential_filter(uint8_t old, uint8_t new, uint8_t alpha)
 	return sum >> 8;
 }
 
+static uint8_t update_digital_state(uint8_t   confidence,
+                                    uint8_t   state,
+                                    uint16_t *num_rising_edges)
+{
+	if (confidence > DIGITAL_LO2HI_THRES) {
+		if (state == 0) {
+			state = 1;
+			(*num_rising_edges)++;
+		}
+	} else if (confidence < DIGITAL_HI2LO_THRES) {
+		if (state == 1) {
+			state = 0;
+		}
+	}
+	return state;
+}
+
+static void read_digital_io(void)
+{
+	uint8_t pins;
+	uint8_t zero_crossing;
+
+	pins = PINE;
+	if ((ticks & ZC_PRESCALER_MASK) == 0) {
+		zero_crossing = PIN_TO_U8(pins, 2);
+		zc_confidence = exponential_filter(zc_confidence,
+		                                   zero_crossing,
+		                                   ZC_ALPHA);
+
+		zc_state = update_digital_state(zc_confidence,
+		                                zc_state,
+		                                &zero_crossings);
+	}
+	if ((ticks & BTN_PRESCALER_MASK) == 0) {
+		uint8_t btn0, btn1;
+		btn0 = PIN_TO_U8(pins, 0);
+		btn1 = PIN_TO_U8(pins, 1);
+
+		btn0_confidence = exponential_filter(btn0_confidence,
+		                                     btn0,
+		                                     BTN_ALPHA);
+		btn1_confidence = exponential_filter(btn1_confidence,
+		                                     btn1,
+		                                     BTN_ALPHA);
+		btn0_state = update_digital_state(btn0_confidence,
+		                                  btn0_state,
+		                                  &btn0_presses);
+		btn1_state = update_digital_state(btn1_confidence,
+		                                  btn1_state,
+		                                  &btn1_presses);
+	}
+}
+
+static void update_display(void)
+{
+	uint8_t which_num = ticks & 0x3;
+	uint8_t brightness_mask = (brightness > rndnum) ? 0xff : 0;
+	brightness 
+
 ISR(TIMER0_OVF_vect)
 {
+	TCNT0 = TMR_RELOAD;
 	update_display();
 	read_digital_io();
 	ticks++;
+	if (ticks & 3 == 0)
+		rerandom();
 }
 
 static void init_vars(void)
 {
+	rnd = { 1, 1, 1, 1 };
 	ticks = 0;
+	zc_state       = btn0_state      = btn1_state      = 0;
+	zc_confidence  = btn0_confidence = btn1_confidence = 0;
+	zero_crossings = btn0_presses    = btn1_presses    = 0;
+	time[0] = time[1] = time[2] = time[3] = time[4] = 0;
 }
 
 static void init_timer_irq(void)
@@ -86,7 +234,8 @@ static inline int read_adc(void)
 
 int main(void)
 {
-	uint16_t next_adc_read = ADC_INTERVAL;
+	uint16_t next_adc_read  = ADC_INTERVAL;
+	uint16_t next_second_zc = ZC_FREQ;
 
 	cli();
 	init_adc();
@@ -96,12 +245,15 @@ int main(void)
 	sei();
 
 	for (;;) {
-		// TODO: 
-
 		if (TIME_AFTER(ticks, next_adc_read)) {
 			// NOTE: Here you have to filter the reading etc
 			if (read_adc())
-				next_adc_read = ticks + ADC_INTERVAL;
+				next_adc_read += ADC_INTERVAL;
 		}
+		if (TIME_AFTER(zero_crossings, next_second_zc)) {
+			advance_second();
+			next_second_zc += ZC_FREQ;
+		}
+		// TODO: handle buttons too
 	}
 }
