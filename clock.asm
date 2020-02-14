f62138659e095788f4c13d079d22cef674b46516
Emmi <emmi@tbd>
2020-02-14 03:45:47 +0200

    it compiles on avra, not tested yet

--- clock.asm
+++ clock.asm
@@ -1,8 +1,5 @@
 .include "m328PBdef.inc"
 
-; TODO TODO: Align number data on a large enough boundary, so that they can
-; never straddle a page boundary (they're 20 bytes, so at least 32B aligned)
-
 .equ	FREQ			= 8000000
 .equ	TMR_FREQ		= ((FREQ) / 1)
 .equ	IRQ_MAXFREQ		= 64000
@@ -19,6 +16,27 @@
 ; is added to TMR_RELOAD at the start of each IRQ, to spread out the IRQ
 ; frequency spikes in output signals.
 .equ	TMR_RELOAD_RANGE	= 15
+
+.equ	BLINK_TOGGLE_FREQ	= 4
+.equ	BLINK_ZC_COUNT		= ((ZC_FREQ) / (BLINK_TOGGLE_FREQ))
+
+.equ	BTN_PRESCALER_MASK	= 0x0f	;  4 kHz
+.equ	ZC_PRESCALER_MASK	= 0x01	; 32 kHz
+
+; Polling ticks (mod 16) for buttons
+.equ	BTN0_TICK		= 0x07
+.equ	BTN1_TICK		= 0x0f
+
+; Note that these use different sampling frequencies
+.equ	ZC_ALPHA		= 32
+.equ	BTN_ALPHA		= 16
+
+.equ	BRIGHTNESS_ALPHA	= 16
+
+; Thresholds for deciding that a digital IO pin appears to have gone
+; low to high, or high to low
+.equ	DIGITAL_LO2HI_THRES	= 200
+.equ	DIGITAL_HI2LO_THRES	= 50
 
 ; TODO: were pfets in PD0...3? Was colon in PD4?
 .equ	PFETS_FORCE_OFF		= 0x0f
@@ -42,6 +60,8 @@
 .def	sreg_cache		= r8
 
 ; And Z here. Do NOT use X for anything!
+.undef	XL
+.undef	XH
 .def	z_cache_lo		= r26
 .def	z_cache_hi		= r27
 
@@ -85,14 +105,20 @@ brpl	@1
 	add	ur2,		ZL
 	ldi	ZL,		high(@3)
 	adc	ur3,		ZL
-	sts	ur3,		@0 + 1
-	sts	ur2,		@0 + 0
+	sts	@0 + 1,		ur3
+	sts	@0 + 0,		ur2
 .endmacro
 
 
 .DSEG
+; The assembler seems to think it can abuse extended IO registers as more
+; SRAM.. No way, I need to have it start at 0x100 so I can have important
+; stuff at page offsets < 0x3f. Who thought an assembler would not need an
+; alignment operand?
+cut_the_crap_john:	.byte 0x100 - 0x60
+
 time:			.byte 5		; u8 * 5; has to be in first 63B
-pfet_masks:		.byte 4		; u8 * 4; ditto, TODO: initialize (0xfe, 0xfd, 0xfb, 0xf7 or something)
+pfet_masks:		.byte 4		; u8 * 4; ditto
 rnd_state:		.byte 3		; u8 * 3 - store rnd.c in register
 zc_confidence:		.byte 1
 btn0_confidence:	.byte 1
@@ -106,6 +132,7 @@ btn1_presses:		.byte 2		; u16
 
 ; User variables
 next_adc_read:		.byte 2		; u16
+next_blink_zc:		.byte 2		; u16
 next_second_zc:		.byte 2		; u16
 btn0_presses_seen:	.byte 2		; u16
 btn1_presses_seen:	.byte 2		; u16
@@ -122,6 +149,7 @@ raw_brightness:		.byte 1
 .equ	mns_hi		= time + 2
 .equ	mns_lo		= time + 3
 .equ	secs		= time + 4
+
 
 
 .CSEG
@@ -163,19 +191,21 @@ ldd	ZL,			Y + low(time)
 ldd	ksp0,			Y + low(pfet_masks)
 
 lsl	ZL
-subi	ZL,			-low(nums)
-ldi	ZH,			high(nums)
+ldi	ZH,			high(nums << 1)
+subi	ZL,			(-low(nums << 1)) & 0xff
+sbci	ZH,			0xff
+
 lpm	num_lo,			Z+
 lpm	num_hi,			Z
 
 lsr	YL
 cp	YL,			blinked_num
-brne	.noblink
+brne	ud_noblink
 	and	num_lo,			blink_mask
 	and	num_hi,			blink_mask
 	ori	num_hi,			COLON_MASK
 
-.noblink:
+ud_noblink:
 and	num_lo,			brightness_mask
 and	num_hi,			brightness_mask
 
@@ -186,9 +216,9 @@ and	num_hi,			ksp0		; save for next one
 read_digital_io:
 mov	tmp_lo,			ticks
 andi	tmp_lo,			ZC_PRESCALER_MASK
-brne	.no_zc_check
+brne	digiio_no_zc_check
 
-.zc_check:
+digiio_zc_check:
 	; Translate ZC bit: 0 -> 0, 1 -> 255
 	clr	tmp_lo
 	sbic	PINE,		2
@@ -198,14 +228,14 @@ brne	.no_zc_check
 	rcall	exponential_filter_isr
 	sts	zc_confidence,	ksp1
 
-	lds	zc_state,	ksp0
+	lds	ksp0,		zc_state
 	ldi	YL,		low(zero_crossings)
 	rcall	update_digital_state_isr
 	sts	zc_state,	ksp0
 	rjmp	isr_return0
 
 ; Assumes that BTN_PRESCALER_MASK > 3 and (BTN_PRESCALER_MASK & 3) == 3
-.no_zc_check:
+digiio_no_zc_check:
 mov	tmp_lo,		ticks
 andi	tmp_lo,		BTN_PRESCALER_MASK
 cpi	tmp_lo,		BTN0_TICK
@@ -225,8 +255,97 @@ isr_return0:
 	out	SREG,		sreg_cache
 	reti	; 128 cycles + IRQ entry latency (7 cycles? maybe 10) thru btn1
 
-; NOTE: this is here just to fit well among other code, as no code flow can
-; just end up here randomly
+btn0_check:
+	; Translate BTN0 bit: 0 -> 0, 1 -> 255
+	clr	tmp_lo
+	sbic	PINE,			0
+	ser	tmp_lo
+
+	lds	ksp0,			btn0_confidence
+	rcall	exponential_filter_isr
+	sts	btn0_confidence,	ksp1
+
+	lds	ksp0,			btn0_state
+	ldi	YL,			low(btn0_presses)
+	rcall	update_digital_state_isr
+	sts	btn0_state,		ksp0
+	rjmp	isr_return0
+
+btn1_check:
+	; Translate BTN1 bit: 0 -> 0, 1 -> 255
+	clr	tmp_lo
+	sbic	PINE,			1
+	ser	tmp_lo
+
+	lds	ksp0,			btn1_confidence
+	; TODO 58
+	rcall	exponential_filter_isr
+	sts	btn1_confidence,	ksp1
+
+	lds	ksp0,			btn1_state
+	ldi	YL,			low(btn1_presses)
+	rcall	update_digital_state_isr
+	; TODO 107
+	sts	btn1_state,		ksp0
+	rjmp	isr_return0
+
+new_brightness_mask:
+	; Use tmp_lo:brightness_disparity_hi as 16-bit correction, note though
+	; that "hi" is lo and "lo" is hi
+	clr	tmp_lo
+	sbrc	brightness_disparity_hi,	7
+	ser	tmp_lo
+
+	; And tmp_hi:tmp_3 as 16-bit brightness, tmp_3 will be corrected
+	; brightness threshold
+	clr	tmp_hi
+	mov	tmp_3,		brightness
+
+	sub	tmp_3,		brightness_disparity_hi
+	sbc	tmp_hi,		tmp_lo
+
+	; If bit 7 is set in tmp_hi, we went negative - use 0 as corrected
+	; threshold. If bit 0 instead is set there, we use 255.
+	sbrc	tmp_hi,		7
+	clr	tmp_3
+	sbrc	tmp_hi,		0
+	ser	tmp_3
+
+	nbm_rerandom:
+	lds	ksp0,		rnd_a
+	lds	ksp1,		rnd_b
+	lds	ksp2,		rnd_x
+
+	inc	ksp2			; rnd.x++
+	eor	ksp0,		rnd_c
+	eor	ksp0,		ksp2	; rnd.a ^= rnd.c ^ rnd.x
+
+	add	ksp1,		ksp0	; rnd.b += rnd.a
+
+	mov	ksp4,		ksp1
+	lsr	ksp4
+	add	rnd_c,		ksp4
+	eor	rnd_c,		ksp0	; rnd.c = rnd.c + (rnd.b >> 1) ^ rnd.a
+
+	sts	rnd_a,		ksp0
+	sts	rnd_b,		ksp1
+	sts	rnd_x,		ksp2
+
+	clr	brightness_mask
+	cp	rnd_c,		tmp_3
+	brcc	nbm_nozero_britemask
+		ser brightness_mask
+
+	nbm_nozero_britemask:
+	clr	ksp2
+	add	brightness_disparity_lo,	brightness_mask
+	adc	brightness_disparity_hi,	ksp2
+
+	sub	brightness_disparity_lo,	brightness
+	sbc	brightness_disparity_lo,	ksp2
+
+	rjmp	isr_return0
+
 ; Params:  ksp0:   old
 ;          tmp_lo: new
 ;          tmp_3:  alpha
@@ -259,143 +378,56 @@ exponential_filter_isr:
 update_digital_state_isr:
 	mov	tmp_lo,		ksp1
 	cpi	tmp_lo,		DIGITAL_LO2HI_THRES
-	brge	.signal_hi
+	brge	uds_signal_hi
 	cpi	tmp_lo,		DIGITAL_HI2LO_THRES
-	brlo	.signal_lo
+	brlo	uds_signal_lo
 	ret
-	.signal_hi:
+	uds_signal_hi:
 		tst	ksp0
-		brne	.was_already_hi
+		brne	uds_was_already_hi
 			inc	ksp0
 			ld	ksp2,	Y+
 			ld	ksp3,	Y
 			dec	YL
 			inc	ksp2
-			brne	.noinchi
+			brne	uds_noinchi
 				inc	ksp3
-			.noinchi:
+			uds_noinchi:
 			st	Y+,	ksp2
 			st	Y,	ksp3
 
-		.was_already_hi:
+		uds_was_already_hi:
 		ret
-	.signal_lo:
+	uds_signal_lo:
 		tst	ksp0
-		breq	.was_already_lo
+		breq	uds_was_already_lo
 			clr	ksp0
-		.was_already_lo:
+		uds_was_already_lo:
 		ret
-
-btn0_check:
-	; Translate BTN0 bit: 0 -> 0, 1 -> 255
-	clr	tmp_lo
-	sbic	PINE,		0
-	ser	tmp_lo
-
-	lds	ksp0,		btn0_confidence
-	rcall	exponential_filter_isr
-	sts	btn0_confidence,	ksp1
-
-	lds	btn0_state,	ksp0
-	ldi	YL,		low(btn0_presses)
-	rcall	update_digital_state_isr
-	sts	btn0_state,	ksp0
-	rjmp	isr_return0
-
-btn1_check:
-	; Translate BTN1 bit: 0 -> 0, 1 -> 255
-	clr	tmp_lo
-	sbic	PINE,		1
-	ser	tmp_lo
-
-	lds	ksp0,		btn1_confidence
-	; TODO 58
-	rcall	exponential_filter_isr
-	sts	btn1_confidence,	ksp1
-
-	lds	btn1_state,	ksp0
-	ldi	YL,		low(btn1_presses)
-	rcall	update_digital_state_isr
-	; TODO 107
-	sts	btn1_state,	ksp0
-	rjmp	isr_return0
-
-new_brightness_mask:
-	; Use tmp_lo:brightness_disparity_hi as 16-bit correction, note though
-	; that "hi" is lo and "lo" is hi
-	clr	tmp_lo
-	sbrc	brightness_disparity_hi,	7
-	ser	tmp_lo
-
-	; And tmp_hi:tmp_3 as 16-bit brightness, tmp_3 will be corrected
-	; brightness threshold
-	clr	tmp_hi
-	mov	tmp_3,		brightness
-
-	sub	tmp_3,		brightness_disparity_hi
-	sbc	tmp_hi,		tmp_lo
-
-	; If bit 7 is set in tmp_hi, we went negative - use 0 as corrected
-	; threshold. If bit 0 instead is set there, we use 255.
-	sbrc	tmp_hi,		7
-	clr	tmp_3
-	sbrc	tmp_hi,		0
-	ser	tmp_3
-
-	.rerandom:
-	lds	ksp0,		rnd_a
-	lds	ksp1,		rnd_b
-	lds	ksp2,		rnd_x
-
-	inc	ksp2			; rnd.x++
-	eor	ksp0,		rnd_c
-	eor	ksp0,		ksp2	; rnd.a ^= rnd.c ^ rnd.x
-
-	add	ksp1,		ksp0	; rnd.b += rnd.a
-
-	mov	ksp4,		ksp1
-	lsr	ksp4
-	add	rnd_c,		ksp4
-	eor	rnd_c,		ksp0	; rnd.c = rnd.c + (rnd.b >> 1) ^ rnd.a
-
-	sts	rnd_a,		ksp0
-	sts	rnd_b,		ksp1
-	sts	rnd_x,		ksp2
-
-	clr	brightness_mask
-	cp	rnd_c,		tmp_3
-	brcc	.nozero_britemask
-		ser brightness_mask
-
-	.nozero_britemask:
-	clr	ksp2
-	add	brightness_disparity_lo,	brightness_mask
-	adc	brightness_disparity_hi,	ksp2
-
-	sub	brightness_disparity_lo,	brightness
-	sbc	brightness_disparity_lo,	ksp2
-
-	rjmp	isr_return0
 
 
 start:
-ldi	SPL,	low (RAMEND)
-ldi	SPH,	high(RAMEND)
+ldi	tmp_lo,	low (RAMEND)
+out	SPL,	tmp_lo
+ldi	tmp_lo,	high(RAMEND)
+out	SPH,	tmp_lo
 
 ldi	YH,	KRNLPAGE
 
-.zero_memory:
+zero_memory:
 ldi	ZL,	low (SRAM_START)
 ldi	ZH,	high(SRAM_START)
 clr	tmp_lo
-.zeroloop:
+zeroloop:
 	st	Z+,	tmp_lo
 	cpi	ZH,	high(SRAM_START + SRAM_SIZE)
-	brlo	.zeroloop
+	brlo	zeroloop
 	cpi	ZL,	low (SRAM_START + SRAM_SIZE)
-	brlo	.zeroloop
+	brlo	zeroloop
 
-.do_pfet_masks:
+ldi	num_hi,		PFETS_FORCE_OFF
+
+do_pfet_masks:
 ldi	tmp_lo,		PFET0_MASK
 sts	pfet_masks + 0,	tmp_lo
 ldi	tmp_lo,		PFET1_MASK
@@ -405,7 +437,7 @@ sts	pfet_masks + 2,	tmp_lo
 ldi	tmp_lo,		PFET3_MASK
 sts	pfet_masks + 3,	tmp_lo
 
-.init_adc:
+init_adc:
 ; Use AVCC, 8-bit ADC, single-ended Channel 0
 ldi	tmp_lo,		0x60
 sts	ADMUX,		tmp_lo
@@ -414,7 +446,7 @@ sts	ADMUX,		tmp_lo
 ldi	tmp_lo,		0xc7
 sts	ADCSRA,		tmp_lo
 
-.init_io:
+init_io:
 ; Ports B and D as outputs, ports C and E as inputs, disable pullups
 ser	tmp_lo
 out	DDRB,		tmp_lo
@@ -429,7 +461,9 @@ out	PORTE,		tmp_lo
 out	PORTB,		tmp_lo
 out	PORTD,		tmp_lo
 
-.init_timer_irq:
+; TODO: check if our power supply has shorted data lines
+
+init_timer_irq:
 ; Prescaler 1, normal mode, etc
 clr	tmp_lo
 out	TCCR0A,		tmp_lo
@@ -445,9 +479,9 @@ ldi	tmp_lo,		1
 sts	TIMSK0,		tmp_lo
 
 
-.init_locals:
 ; Assumes that ADC_INTERVAL and ZC_FREQ fit in 8 bits and SRAM is already
 ; cleared
+init_locals:
 ldi	tmp_lo,		ADC_INTERVAL
 sts	next_adc_read,	tmp_lo
 ldi	tmp_lo,		ZC_FREQ
@@ -466,7 +500,7 @@ mainloop:
 	; kernel than how they're being iterated over here). It is a much more
 	; benign glitch than accidentally getting 37ff instead, because then
 	; we would be 255 variable updates early.
-	.check_adc_time:
+	ml_check_adc_time:
 	lds	ur1,		zero_crossings + 1
 	lds	ur0,		zero_crossings + 0
 
@@ -474,38 +508,41 @@ mainloop:
 	lds	ur2,		next_adc_read + 0
 
 	rcall	time_after
-	brpl	.check_blink_time
+	brpl	ml_check_blink_time
 
 		rcall	read_adc_adjust_brightness
-		brcs	.check_blink_time
+		brcs	ml_check_blink_time
 			; Increment ur2:ur3
 			ldi	ZL,		ADC_INTERVAL
 			add	ur2,		ZL
 			ldi	ZL,		0
 			adc	ur3,		ZL
-			sts	ur3,		next_adc_read + 1
-			sts	ur2,		next_adc_read + 0
+			sts	next_adc_read + 1,	ur3
+			sts	next_adc_read + 0,	ur2
 
 ; Params: ur0:ur1 timer to compare against target timestamp
 ;         @0      memory address of target timestamp variable
 ;         @1      escape label
 ;         @2      function to call if timer hass advanced past *(@0)
 ;         @3      event interval in timer ticks
-	.check_blink_time:
-	user_check_time_after	next_blink_zc,		.check_next_second_time, toggle_blink, BLINK_ZC_COUNT
+	ml_check_blink_time:
+	user_check_time_after	next_blink_zc,		ml_check_next_second_time, toggle_blink, BLINK_ZC_COUNT
 
-	.check_next_second_time:
-	user_check_time_after	next_second_zc,		.check_btn0_presses,	advance_second, ZC_FREQ
+	ml_check_next_second_time:
+	user_check_time_after	next_second_zc,		ml_check_btn0_presses,	advance_second, ZC_FREQ
 
-	.check_btn0_presses:
+	ml_check_btn0_presses:
 	lds	ur3,		btn0_presses + 1
 	lds	ur2,		btn0_presses + 0
-	user_check_time_after	btn0_presses_seen,	.check_btn1_presses,	advance_blink, 1
+	user_check_time_after	btn0_presses_seen,	ml_check_btn1_presses,	advance_blink, 1
 
-	.check_btn1_presses:
+	ml_check_btn1_presses:
 	lds	ur3,		btn1_presses + 1
 	lds	ur2,		btn1_presses + 0
-	user_check_time_after	btn1_presses_seen,	mainloop,		advance_blinked_num, 1
+	user_check_time_after	btn1_presses_seen,	ml_continue,		advance_blinked_num, 1
+
+	ml_continue:
+	rjmp mainloop
 
 ; ur0:ur1: timestamp A
 ; ur2:ur3: timestamp B
@@ -563,7 +600,7 @@ read_adc_adjust_brightness:
 
 	lds	ur0,		ADCSRA
 	sbrc	ur0,		ADSC		; Still converting?
-		rjmp		.out_carry
+		rjmp		adc_out_carry
 
 	clr	ur0
 	dec	ur0
@@ -576,29 +613,29 @@ read_adc_adjust_brightness:
 	rcall	exponential_filter_user
 
 	cp	ur3,		ur1
-	breq	.noupdate
+	breq	adc_noupdate
 		sts	raw_brightness,	ur3
 
 		; Get actual brightness value from table
 		clr	ur1
 		; Read corresponding entry from brightness table
-		ldi	ZL,		low (brightness_table)
-		ldi	ZH,		high(brightness_table)
+		ldi	ZL,		low (brightness_table << 1)
+		ldi	ZH,		high(brightness_table << 1)
 		add	ZL,		ur3
 		adc	ZH,		ur1
-		lpm	brightness
+		lpm	brightness,	Z
 
 	; Start reading next value
-	.noupdate:
+	adc_noupdate:
 	lds	ZL,		ADCSRA
 	ori	ZL,		(1 << ADSC)
 	clc
-	rjmp	.out
+	rjmp	adc_out
 
-	.out_carry:
+	adc_out_carry:
 		sec
 
-	.out:
+	adc_out:
 	pop	ZH
 	pop	ZL
 	pop	ur3
@@ -608,7 +645,7 @@ read_adc_adjust_brightness:
 	ret
 
 toggle_blink:
-	not	blink_mask
+	com	blink_mask
 	ret
 
 advance_blink:
@@ -616,9 +653,9 @@ advance_blink:
 	mov	ZL,		blinked_num
 	inc	ZL
 	cpi	ZL,		3
-	brlo	.out
+	brlo	blink_out
 		clr	ZL
-	.out:
+	blink_out:
 	mov	blinked_num,	ZL
 	pop	ZL
 	ret
@@ -627,17 +664,17 @@ advance_blinked_num:
 	push	ZL
 	mov	ZL,	blinked_num
 	tst	ZL
-	brne	.no_advance_hour
+	brne	abn_no_advance_hour
 		rcall	advance_hour
-		rjmp	.out
+		rjmp	abn_out
 
-	.no_advance_hour:
+	abn_no_advance_hour:
 	cpi	ZL,	1
-	brne	.out
+	brne	abn_out
 		clr	ZL
 		sts	secs,		ZL
 		rcall	advance_minute
-	.out:
+	abn_out:
 	pop	ZL
 	ret
 
@@ -646,10 +683,10 @@ advance_second:
 	lds	ZL,		secs
 	inc	ZL
 	cpi	ZL,		60
-	brlo	.no_minute
+	brlo	as_no_minute
 		clr	ZL
 		rcall	advance_minute
-	.no_minute:
+	as_no_minute:
 	sts	secs,		ZL
 	pop	ZL
 	ret
@@ -659,24 +696,24 @@ advance_minute:
 	lds	ZL,		mns_lo
 	inc	ZL
 	cpi	ZL,		10
-	brge	.do_tens
+	brge	am_do_tens
 		sts	mns_lo,		ZL
-		rjmp	.out
+		rjmp	am_out
 
-	.do_tens:
+	am_do_tens:
 		clr	ZL
 		sts	mns_lo,		ZL
 		lds	ZL,		mns_hi
 		inc	ZL
 		cpi	ZL,	6
-		brge	.do_hour
+		brge	am_do_hour
 			sts	mns_hi,		ZL
-			rjmp	.out
-		.do_hour:
+			rjmp	am_out
+		am_do_hour:
 			clr	ZL
 			sts	mns_hi,		ZL
 			rcall	advance_hour
-	.out:
+	am_out:
 	pop	ZL
 	ret
 
@@ -688,19 +725,42 @@ advance_hour:
 	lds	ZH,	hrs_hi
 	inc	ZL
 	cpi	ZL,	10
-	brlo	.no_tens
+	brlo	ah_no_tens
 		clr	ZL
 		inc	ZH
-	.no_tens:
+	ah_no_tens:
 		cpi	ZL,	4
-		brlo	.out
+		brlo	ah_out
 			cpi	ZH,	2
-			brlo	.out
+			brlo	ah_out
 				clr	ZL
 				clr	ZH
-	.out:
+	ah_out:
 	sts	hrs_lo,	ZL
 	sts	hrs_hi,	ZH
 	pop	ZH
 	pop	ZL
 	ret
+
+nums:
+.db 0xff, 0x03, 0x3c, 0x00, 0xdf, 0x05, 0x7f, 0x05, 0x39, 0x07	; 0...4
+.db 0x6f, 0x07, 0xef, 0x07, 0x3f, 0x04, 0xff, 0x07, 0x7f, 0x07	; 5...9
+
+; TODO :D
+brightness_table:
+.db 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f
+.db 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
+.db 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28, 0x29, 0x2a, 0x2b, 0x2c, 0x2d, 0x2e, 0x2f
+.db 0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3a, 0x3b, 0x3c, 0x3d, 0x3e, 0x3f
+.db 0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4a, 0x4b, 0x4c, 0x4d, 0x4e, 0x4f
+.db 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5a, 0x5b, 0x5c, 0x5d, 0x5e, 0x5f
+.db 0x60, 0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6a, 0x6b, 0x6c, 0x6d, 0x6e, 0x6f
+.db 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7a, 0x7b, 0x7c, 0x7d, 0x7e, 0x7f
+.db 0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f
+.db 0x90, 0x91, 0x92, 0x93, 0x94, 0x95, 0x96, 0x97, 0x98, 0x99, 0x9a, 0x9b, 0x9c, 0x9d, 0x9e, 0x9f
+.db 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf
+.db 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xb5, 0xb6, 0xb7, 0xb8, 0xb9, 0xba, 0xbb, 0xbc, 0xbd, 0xbe, 0xbf
+.db 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf
+.db 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0xd5, 0xd6, 0xd7, 0xd8, 0xd9, 0xda, 0xdb, 0xdc, 0xdd, 0xde, 0xdf
+.db 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6, 0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee, 0xef
+.db 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff
